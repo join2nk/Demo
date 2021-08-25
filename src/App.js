097e3a1f9e48fe420881a123ea5531e0fe7d28aa
Nikhil Kumar <nknapster@gmail.com>
2021-08-25 18:49:03 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React by Nikhil
+          Learn React by Nikhil...
         </a>
       </header>
     </div>
